1f259536aa38da193d94acdba82b1d0f933fb2bd
Jonas Schievink <jonasschievink@gmail.com>
2020-02-09 16:09:00 +0100

    Remove `Peripheral`
    It appears to be unused by cortex-m and generated PACs

--- src/lib.rs
+++ src/lib.rs
@@ -7,36 +7,6 @@
 use core::cell::UnsafeCell;
 use core::marker::PhantomData;
 
-/// A peripheral
-#[derive(Debug)]
-pub struct Peripheral<T>
-where
-    T: 'static,
-{
-    address: *mut T,
-}
-
-impl<T> Peripheral<T> {
-    /// Creates a new peripheral
-    ///
-    /// `address` is the base address of the register block
-    pub const unsafe fn new(address: usize) -> Self {
-        Peripheral {
-            address: address as *mut T,
-        }
-    }
-
-    /// Borrows the peripheral for the duration of a critical section
-    pub fn borrow<'cs>(&self, _ctxt: CriticalSection<'cs>) -> &'cs T {
-        unsafe { &*self.get() }
-    }
-
-    /// Returns a pointer to the register block
-    pub fn get(&self) -> *mut T {
-        self.address as *mut T
-    }
-}
-
 /// Critical section token
 ///
 /// Indicates that you are executing code within a critical section
